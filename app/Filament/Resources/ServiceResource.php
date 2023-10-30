492c6f5fd9f9acacdb48e95f262f1bd87721b8cb
kaled molina <kaled.molina@campusucc.edu.co>
2023-10-30 17:00:07 -0500

    servicio vista modelo y componente home

---
+++ app/Filament/Resources/ServiceResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Tables;
+use App\Models\Service;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\RichEditor;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ServiceResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ServiceResource\RelationManagers;
+
+class ServiceResource extends Resource
+{
+    protected static ?string $model = Service::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->label(__('titulo'))
+                    ->autofocus()
+                    ->required()
+                    ->placeholder(__('titulo')),
+                TextInput::make('icon_class')
+                    ->label(__('icon_class'))
+                    ->autofocus()
+                    ->placeholder(__('icon_class')),
+                TextInput::make('short_desc')
+                    ->label(__('short_desc'))
+                    ->autofocus()
+                    ->required()
+                    ->placeholder(__('short_desc')),
+                RichEditor::make('descripcion')
+                    ->label(__('descripcion'))
+                    ->autofocus()
+                    ->required()
+                    ->placeholder(__('descripcion'))
+                    ->columnSpan(2),
+                Select::make('status')
+                    ->label(__('status'))
+                    ->autofocus()
+                    ->required()
+                    ->placeholder(__('status'))
+                    ->options([
+                        1 => __('activo'),
+                        0 => __('inactivo'),
+                    ]),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListServices::route('/'),
+            'create' => Pages\CreateService::route('/create'),
+            'edit' => Pages\EditService::route('/{record}/edit'),
+        ];
+    }
+}
